ba6fd75788d4083de579807ebd3bfcc72dcc1923
stephp23 <stephp23@gmail.com>
2020-12-10 13:24:17 -0500

    changes to js and html files

--- ducky.js
+++ ducky.js
@@ -8,18 +8,17 @@ window.onload = function() {
   //   2. add a class to the element
   //   3. append the element to the body )
 
-  let elementDuck = document.createElement("div");
-  elementDuck.classList.add("duck");
-  document.body.append(duck);
+  // let elementDuck = document.createElement("div");
+  // elementDuck.classList.add("duck");
+  // document.body.append(duck);
 
   // 2. Next, use setInterval to toggle the "flap" class on the duck every 250 ms (1/4 second)
   // https://www.w3schools.com/jsref/met_win_setinterval.asp
 
-  setInterval(function () {
-    elementDuck.classList.toggle("flap");
-  }, 250);
-
-  
+  // let duckWingsFlapping = setInterval(function wingFlappin() {
+  //   elementDuck.classList.toggle("flap");
+  // }, 250);
+ 
 
   // 3. Now, let's move the duck using CSS "top" and "left". Create
   // a function `moveDuck` that takes a duck object as an argument and sets the
@@ -27,7 +26,20 @@ window.onload = function() {
   // HINT: Use Math.random() * window.innerWidth    for "left"
   //       And Math.random() * window.innerHeight   for "top"
 
+//   function moveDuck() {
+//     elementDuck.style.top = Math.random() * window.innerHeight + "px";
+//     elementDuck.style.left = Math.random() * window.innerWidth + "px";
+// }
+
+
   // 4. Try making the duck move to a different location every second (what did we use to do this several lines up??)
+
+  // let moveDuckDiffLoc = setInterval(moveDuck, 1000);
+
+
+
+
+
 
   // 5. Congratulations! Move on to part 2!
 
@@ -37,9 +49,37 @@ window.onload = function() {
   //    a "function" called createDuck() that does everything in 1-4
   //    and "returns" the duck object
 
-  // 7. Now, let's create lots of ducks!  Use a "for" loop to create 5 ducks
-  //    using our fancy new createDuck() function
+   //    using our fancy new createDuck() function
 
+  function createDuck() { 
+     
+    let elementDuck = document.createElement("div");
+    elementDuck.classList.add("duck");    
+    document.body.appendChild(duckElement);
+
+    let duckWingsFlapping = setInterval(function wingFlappin() {
+      elementDuck.classList.toggle("flap");
+    }, 250);
+
+    function moveDuck() {
+      elementDuck.style.top = Math.random() * window.innerHeight + "px";
+      elementDuck.style.left = Math.random() * window.innerWidth + "px";
+    }
+
+    //#8
+    function randomPosition() {
+      let randomDuckHeight = Math.floor(Math.random() * window.innerHeight);
+      let randomDuckWidth = Math.floor(Math.random() * window.innerWidth);
+
+      duck.style.top = `${randomDuckHeight}px`;
+      duck.style.left = `${randomDuckWidth}px`;
+     
+    }
+
+    //#11
+
+
+  
   // 8. The ducks are overlapping.  Modify createDuck so each time
   //     it creates a duck, it appears in a random location
   // HINT: You may want to create a `randomPosition()` function that you can use
